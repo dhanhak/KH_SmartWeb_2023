26e2b31a4f70d232b549c61453c46c00b0a82a2c
dhanhak <dhanhak@naver.com>
2023-03-30 15:49:17 +0900

    퀴즈 업데이트

--- workspace_webtest/WebTest_01/src/quiz_01.java
+++ workspace_webtest/WebTest_01/src/quiz_01.java
@@ -3,6 +3,8 @@ import java.time.Duration;
 import java.util.List;
 import java.util.Set;
 
+import javax.lang.model.element.Element;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
@@ -16,14 +18,14 @@ public class quiz_01 {
 	public static void main(String[] args) throws Exception {
 		WebDriver driver = new ChromeDriver();
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-		
+
 		driver.manage().window().setSize(new Dimension(1700, 900));
 		driver.get("https://www.monamhee.com/");
-		
+
 		driver.findElement(By.linkText("로그인 위젯 문구")).click();
 		driver.findElement(By.name("uid")).sendKeys(Statics.monamhee_ID);
 		driver.findElement(By.name("passwd")).sendKeys(Statics.monamhee_PW);
-		
+
 		driver.findElement(By.className("btn-primary")).click();
 
 		Thread.sleep(500);
@@ -34,25 +36,36 @@ public class quiz_01 {
 		driver.get("https://www.monamhee.com/grocerystore/?idx=78");
 		Thread.sleep(500);
 		driver.findElement(By.className("bt-plus")).click();
-		driver.findElement(By.className("bt-plus")).click();
 		driver.findElement(By.linkText("구매하기")).click();
-		
+
 		Thread.sleep(500);
 		driver.findElement(By.name("deliv_name")).sendKeys("권지수");
-//		driver.findElement(By.name("deliv_call")).sendKeys("010-5213-4666");
-//		driver.findElement(By.linkText("주소찾기")).click();
-//		
-//		driver.findElement(By.name("deliv_name")).sendKeys("청계그레이츠");
-//		
-//		driver.findElement(By.linkText("서울 중구 남대문로 120 (그레이츠 청계)")).click();
-//		
-//		driver.findElement(By.linkText("결제하기")).click();
-		
-			
-		}
-		
-		
-	
+		Thread.sleep(500);
+
+		List<WebElement> deliCall = driver.findElements(By.name("deliv_call"));
+		deliCall.get(0).sendKeys("010-5213-4666");
+		Thread.sleep(500);
+		driver.findElement(By.linkText("주소찾기")).click();
+
+		WebElement outerFrame = driver.findElement(By.cssSelector("#__daum__layer_1 > iframe"));
+
+		WebElement innerFrame = driver.switchTo().frame(outerFrame).findElement(By.tagName("iframe"));
+
+		driver.switchTo().frame(innerFrame).findElement(By.id("region_name")).sendKeys("청계그레이츠");
+		Thread.sleep(500);
+		driver.findElement(By.className("btn_search")).click();
+//		wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.className("link_post"), 2));
+		driver.findElements(By.className("link_post")).get(2).click();
+		Thread.sleep(500);
+		driver.findElement(By.name("shop_form[f202303099e5d85055beb9]")).click();
+		driver.findElement(By.id("paymentAllCheck")).click();
+		driver.findElement(By.linkText("결제하기")).click();
+
+
+	}
+
+
+
 }
 
 
